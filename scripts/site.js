b50b52300bf34c401f8c2dfc7535d1c2c000bc0c
Tristanx <Tristanx>
2018-08-05 00:59:56 +1000

    Add share menu and quote

--- scripts/site.js
+++ scripts/site.js
@@ -1,9 +1,15 @@
 // mobile nav toggle
 var $header = document.getElementById('site-header'),
     $toggle = document.querySelector('#site-header .nav-toggle'),
+    $share = document.querySelector('#site-header .share'),
+    $shareList = document.querySelector('#site-header .share-list'),
     $nav = document.querySelector('#site-header nav');
 
 $toggle.addEventListener('click', function() {
     $header.classList.toggle('nav-visible');
     $nav.style.height = document.body.clientHeight + 'px';
+});
+
+$share.addEventListener('click', function() {
+    $shareList.classList.toggle('share-visible');
 });
